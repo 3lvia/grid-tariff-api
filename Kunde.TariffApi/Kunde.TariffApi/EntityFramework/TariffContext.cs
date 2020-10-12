fc5e13e7281e3753dfba9f9535bd1079e6fe30d2
Espen Johan Håvås <EspenJohan.Havas@eidsiva.no>
2020-10-12 11:35:28 +0200

    removed hardcoded connectionstring from entityframeworkgeneration process.

--- Kunde.TariffApi/Kunde.TariffApi/EntityFramework/TariffContext.cs
+++ Kunde.TariffApi/Kunde.TariffApi/EntityFramework/TariffContext.cs
@@ -29,11 +29,6 @@ namespace Kunde.TariffApi.EntityFramework
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("server=localhost;Initial Catalog=Elvia_effektAPITest_0_06;integrated security=true;");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
