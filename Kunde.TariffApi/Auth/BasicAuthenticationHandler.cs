e3b4a7e194b94b15a308bcc5b86db9b6c9529c71
Espen Johan Håvås <EspenJohan.Havas@eidsiva.no>
2020-11-04 14:32:16 +0100

    Elvid authentication replaced with basic authentication for use with Azure API Management.

---
+++ Kunde.TariffApi/Auth/BasicAuthenticationHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Kunde.TariffApi.Auth
+{
+    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly AuthenticationConfig _config;
+
+        public BasicAuthenticationHandler(
+                    IOptionsMonitor<AuthenticationSchemeOptions> options,
+                    ILoggerFactory logger,
+                    UrlEncoder encoder,
+                    ISystemClock clock,
+                    AuthenticationConfig config)
+                    : base(options, logger, encoder, clock)
+        {
+            _config = config;
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            return await Task.Run(() =>
+            {
+                if (!Request.Headers.ContainsKey("Authorization"))
+                    return AuthenticateResult.Fail("Missing Authorization Header");
+
+                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+                if (authHeader.Scheme.ToLower() != "basic")
+                {
+                    return AuthenticateResult.NoResult();
+                }
+                var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':', 2);
+                if (credentials.Length != 2)
+                {
+                    return AuthenticateResult.Fail("Error in Authorization Header");
+                }
+                var username = credentials[0];
+                var password = credentials[1];
+                if (username != _config.Username || password != _config.Password)
+                {
+                    return AuthenticateResult.Fail("Invalid Authorization Header");
+                }
+
+                var identity = new ClaimsIdentity(Scheme.Name);
+                var principal = new ClaimsPrincipal(identity);
+                var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+                return AuthenticateResult.Success(ticket);
+            });
+        }
+    }
+}
