bc2fa65eb34f55291979cb7790265ba8c698326b
Espen Johan Håvås <EspenJohan.Havas@eidsiva.no>
2022-02-18 13:39:33 +0100

    Serialize Exception.

--- GridTariffAPI/Exceptions/GridTariffApiException.cs
+++ GridTariffAPI/Exceptions/GridTariffApiException.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace GridTariffApi.Exceptions
 {
+    [Serializable]
     public class GridTariffApiException : Exception
     {
-        public GridTariffApiException(string message, Exception inner)
+        public GridTariffApiException()
+        {
+        }
+
+        public GridTariffApiException(string message, Exception inner = null)
             : base(message, inner)
+        {
+        }
+
+        protected GridTariffApiException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
