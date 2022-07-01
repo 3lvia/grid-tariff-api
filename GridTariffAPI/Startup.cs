d595862b00c249f7338c3f32426f7dd7f68b14f3
Jonas Bjelkerud <jbg@computas.com>
2022-07-01 18:43:30 +0200

    GLUE-1812: prøver igjen

--- GridTariffAPI/Startup.cs
+++ GridTariffAPI/Startup.cs
@@ -148,7 +148,7 @@ namespace GridTariffApi
                 services.AddCronJob<MeteringPointTariffSynchronizer>(c =>
                 {
                     c.TimeZoneInfo = NorwegianTimeZoneInfo();
-                    c.CronExpression = @"42 0/1 * * *"; // every hour
+                    c.CronExpression = @"48 0/1 * * *"; // every hour
                 });
             }
 
